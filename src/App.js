b5efaab9e858e88e501580978ae51fcb34fa375d
Ivan Weber <ivanbdn@yandex.ru>
2020-09-29 08:26:25 +0300

    useContext added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './Todo/TodoList'
+import Context from './context'
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -24,10 +25,19 @@ function App() {
     )
   }
 
-  return (<div className="wrapper">
-    <h1>React tutorial</h1>
-    <TodoList todos={todos} onToggle={toggleTodo}/>
-  </div>)
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+
+  } 
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <TodoList todos={todos} onToggle={toggleTodo}/>
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App;
