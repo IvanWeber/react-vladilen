7f4769f91216c7e6ab999884f57137c7e61a31f1
Ivan Weber <ivanbdn@yandex.ru>
2020-09-29 09:01:46 +0300

    AddTodos created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './Todo/TodoList'
 import Context from './context'
+import AddTodo from './Todo/AddTodo';
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -17,7 +18,7 @@ function App() {
   function toggleTodo(id) {
     setTodos(
       todos.map(todo => {
-        if (todo.id === id) {
+        if (todo.id === id) { 
           todo.completed = !todo.completed
         }
         return todo
@@ -30,10 +31,21 @@ function App() {
 
   } 
 
+  function addTodo (title) {
+    setTodos(todos.concat([{
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo}/>
          ) : (
